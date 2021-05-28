3aed0ca847e1e77ffbde16c990ca4e9d32fc1065
Amirul Mujahidin <73012169+amiruldev20@users.noreply.github.com>
2021-05-28 14:04:11 +0700

    Create fetcher.js
    gusah diedit ntr eror nangid

--- renz/lib/fetcher.js
+++ renz/lib/fetcher.js
@@ -1,9 +1,11 @@
 /*
-|===========================================|
-| FETCHER - NODE FETCH                      |
-| CREATOR: AMIRUL DEV                       |
-| WEB: WWW.AMIRULDEV.NET                    |
-|===========================================|
+|=======================================|
+| NAME: EXAMPLE API                     |
+| VERSION: 0.0.1                        |
+| CREATOR: AMIRUL DEV                   |
+| WEB: WWW.AMIRULDEV.NET                |
+| TEMPLATE BY: MBAH AGUS                |
+|=======================================|
 */
 const fetch = require('node-fetch')
 const fs = require('fs')
